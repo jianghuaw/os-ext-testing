94457760272fd8acfa1562b17b113cb4525edde1
Bob Ball <bob.ball@citrix.com>
2016-02-12 16:31:54 +0000

    Add nodepool scripts from infra/project-config c93c0dfd10906a8cf5b4b9d67c07983d4315edd8

--- install_master.sh
+++ install_master.sh
@@ -5,17 +5,25 @@ THIS_DIR=`pwd`
 DATA_PATH=/root/os-ext-data
 
 # Steps to reinstall:
-# 1) Create new Ubuntu 14.04 server (copy password)
-# 1a) 7.5GB Compute v1 flavor
-# 1b) Enable monitoring and security updates
-# 2) Disable password authentication
+# 1) Log in to mycloud.rackspace.com using credentials from os-ext-data/xenlibvirt-nodepool
+# 1a) Create new Ubuntu 14.04 server (copy password), hostname 'jenkins-libvirt'
+#     7.5GB Compute v1 flavor
+#     Enable monitoring and security updates
+# 1b) Save the password for use in step 2a
+# 1c) Add key from os-ext-data/xenproject_jenkins.pub with the name 'xenproject-nodepool'
+# 2) Disable password authentication on jenkins server
 # 2a) ssh-copy-id to copy a key to the server
 # 2b) edit /etc/sshd_config to set "PermitRootLogin without-password"
+# 2c) service ssh restart
 # 3) Copy the secret credentials dir (http://hg.uk.xensource.com/openstack/infrastructure.hg/os-ext-data) to /root
-# 4) Clone this repo git:
-# 4a) git clone https://github.com/citrix-openstack/os-ext-testing.git
-# 4b) cd os-ext-testing; git checkout common_ci
-# ?) Follow steps below
+# 4) Clone this repo:
+# 4a) apt-get install git
+# 4b) git clone https://github.com/citrix-openstack/os-ext-testing.git
+# 4c) cd os-ext-testing; git checkout common_ci
+# 5) Run below commands (or just this script) to do the 'standard' install
+# 6) The jobs need an additional plugin in Jenkins to generate correctly, so:
+# 6a) Install Post-Build Script jenkins plugin (including restarting Jenkins)
+# 6b) Regenerate jenkins jobs: jenkins-jobs update --delete-old /etc/jenkins_jobs/config
 # ?) Set up monitoring checks https://intelligence.rackspace.com/cloud/entities/enWCIYVVnt
 
 # Copied from the following URL Feb 2016
@@ -48,3 +56,4 @@ cp /root/os-ext-data/single_node_ci_data.yaml /etc/puppet/environments/common.ya
 sed -i -e 's/^\(127\.0\.0\.1.*\)$/\1 jenkins/' /etc/hosts
 
 sudo puppet apply --verbose /etc/puppet/manifests/site.pp
+
---
+++ nodepool/scripts/configure_mirror.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -xe
+
+# Copyright (C) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source /etc/nodepool/provider
+
+# Generate the AFS Slug from the host system.
+source /usr/local/jenkins/slave_scripts/afs-slug.sh
+
+NODEPOOL_MIRROR_HOST=${NODEPOOL_MIRROR_HOST:-mirror.$NODEPOOL_REGION.$NODEPOOL_CLOUD.openstack.org}
+NODEPOOL_MIRROR_HOST=$(echo $NODEPOOL_MIRROR_HOST|tr '[:upper:]' '[:lower:]')
+NODEPOOL_PYPI_MIRROR=${NODEPOOL_PYPI_MIRROR:-http://$NODEPOOL_MIRROR_HOST/pypi/simple}
+NODEPOOL_WHEEL_MIRROR=${NODEPOOL_WHEEL_MIRROR:-http://$NODEPOOL_MIRROR_HOST/wheel/$AFS_SLUG}
+NODEPOOL_UBUNTU_MIRROR=${NODEPOOL_UBUNTU_MIRROR:-http://$NODEPOOL_MIRROR_HOST/ubuntu}
+
+cat >/tmp/pip.conf <<EOF
+[global]
+timeout = 60
+index-url = $NODEPOOL_PYPI_MIRROR
+trusted-host = $NODEPOOL_MIRROR_HOST
+extra-index-url = $NODEPOOL_WHEEL_MIRROR
+EOF
+sudo mv /tmp/pip.conf /etc/pip.conf
+
+cat >/home/jenkins/.pydistutils.cfg <<EOF
+[easy_install]
+index_url = $NODEPOOL_PYPI_MIRROR
+allow_hosts = *.openstack.org
+EOF
+
+# Double check that when the node is made ready it is able
+# to resolve names against DNS.
+host git.openstack.org
+host $NODEPOOL_MIRROR_HOST
+
+LSBDISTID=$(lsb_release -is)
+LSBDISTCODENAME=$(lsb_release -cs)
+if [ "$LSBDISTID" == "Ubuntu" ] ; then
+    sudo dd of=/etc/apt/sources.list <<EOF
+deb $NODEPOOL_UBUNTU_MIRROR $LSBDISTCODENAME main universe
+deb $NODEPOOL_UBUNTU_MIRROR $LSBDISTCODENAME-updates main universe
+deb $NODEPOOL_UBUNTU_MIRROR $LSBDISTCODENAME-backports main universe
+deb $NODEPOOL_UBUNTU_MIRROR $LSBDISTCODENAME-security main universe
+EOF
+    if [ "$LSBDISTCODENAME" != 'precise' ] ; then
+        # Turn off multi-arch
+        sudo dpkg --remove-architecture i386
+    fi
+    # Turn off checking of GPG signatures
+    sudo dd of=/etc/apt/apt.conf.d/99unauthenticated <<EOF
+APT::Get::AllowUnauthenticated "true";
+EOF
+fi
